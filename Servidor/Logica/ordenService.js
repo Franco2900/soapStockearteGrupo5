42abfe1100aeda5f23382032e93fbfd84224587e
Franco2900 <franco2900@gmail.com>
2024-11-01 16:37:23 -0300

    la consulta de ordenes de compra ahora discrimina por tipo de usuario

--- Servidor/Logica/ordenService.js
+++ Servidor/Logica/ordenService.js
@@ -44,22 +44,41 @@ async function consultarOrdenesDeCompra(args)
 
     try
     {
-        // AÑADIR: 
-        // SI EL USUARIO ES DE TIENDA CENTRAL, SE USA LA tienda_codigo QUE MANDO EL USUARIO (SI NO MANDO NINGUN tienda_codigo SE CONSULTA TODAS LAS TIENDAS)
-        // SI EL USUARIO ES DE TIENDA COMÚN, SE USA LA tienda_codigo DEL USUARIO
+        // Consigo los datos
+        var usuario = args.usuario;
+        var producto_codigo = args.producto_codigo;
+        var estado = args.estado;
+        var fecha_inicio = args.fecha_inicio;
+        var fecha_final = args.fecha_final;
+        var tienda_codigo = args.tienda_codigo;
 
+        // Consulto el tipo de usuario
+        let consultaTipoDeUsuario = await conexionDataBase.query(`SELECT central, codigo 
+            FROM tienda 
+            INNER JOIN usuario
+            ON codigo = tienda_codigo
+            WHERE usuario = '${usuario}' `, {});
+
+        console.log(Boolean(consultaTipoDeUsuario[0].central) ); // DEBUG
+        
+        // Consulto las ordenes de compra
         let consultaSQL = `SELECT orden_de_compra.id AS id, estado, observaciones, fecha_de_solicitud, fecha_de_recepcion, tienda_codigo, producto_codigo, SUM(cantidad_solicitada) AS cantidad_solicitada
             FROM orden_de_compra 
             INNER JOIN item ON orden_de_compra.id = item.id_orden_de_compra
             WHERE 1=1`;
 
-        if(args.producto_codigo) consultaSQL += ` AND item.producto_codigo = '${args.producto_codigo}' `;
-        if(args.estado)          consultaSQL += ` AND orden_de_compra.estado = '${args.estado}' `;
-        if(args.fecha_inicio)    consultaSQL += ` AND fecha_de_solicitud >= '${args.fecha_inicio}' `;
-        if(args.fecha_final)     consultaSQL += ` AND fecha_de_solicitud <= '${args.fecha_final}' `;
-        if(args.tienda_codigo)   consultaSQL += ` AND tienda_codigo = '${args.tienda_codigo}' `;
+        if(producto_codigo) consultaSQL += ` AND item.producto_codigo = '${producto_codigo}' `;
+        if(estado)          consultaSQL += ` AND orden_de_compra.estado = '${estado}' `;
+        if(fecha_inicio)    consultaSQL += ` AND fecha_de_solicitud >= '${fecha_inicio}' `;
+        if(fecha_final)     consultaSQL += ` AND fecha_de_solicitud <= '${fecha_final}' `;
+        
+        if(Boolean(consultaTipoDeUsuario[0].central) ) // Si es usuario de tienda central
+            if(tienda_codigo)   consultaSQL += ` AND tienda_codigo = '${tienda_codigo}' `; // Se usa la tienda_codigo que mando el usuario (si no mando ningún tienda_codigo, se consulta todas las tiendas)
+        
+        else // Si es usuario de tienda comun
+            consultaSQL += ` AND tienda_codigo = '${consultaTipoDeUsuario[0].codigo}' `;   // Se usa la tienda_codigo del usuario
 
-        consultaSQL += `GROUP BY producto_codigo, estado, tienda_codigo`
+        consultaSQL += ` GROUP BY producto_codigo, estado, tienda_codigo`;
 
         //console.log(consultaSQL); //DEBUG
 
